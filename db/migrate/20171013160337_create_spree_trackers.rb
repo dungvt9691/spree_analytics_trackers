30e18147bb965e320c29737ea7219de22159fc9e
dungvt9691 <dungvt9691@gmail.com>
2019-06-11 00:06:04 +0700

    Update 20171013160337_create_spree_trackers.rb

--- db/migrate/20171013160337_create_spree_trackers.rb
+++ db/migrate/20171013160337_create_spree_trackers.rb
@@ -1,4 +1,4 @@
-class CreateSpreeTrackers < SpreeExtension::Migration[4.2]
+class CreateSpreeTrackers < ActiveRecord::Migration
   def change
     if table_exists?(:spree_trackers)
       add_index :spree_trackers, :active unless index_exists?(:spree_trackers, :active)
